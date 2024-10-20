a82a54fb98a7e551c1b6ac7fd9a31ab091a8b067
Tomasz Jąder <tomasz.jader@gmail.com>
2024-10-20 23:29:45 +0200

    update angular

--- notatnik-frontend/src/app/app.module.ts
+++ notatnik-frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ReactiveFormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [AppComponent, NoteListComponent, NoteFormComponent],
@@ -31,8 +32,9 @@ import { ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule,
     BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent],
-  entryComponents: [NoteFormComponent],
 })
 export class AppModule {}
