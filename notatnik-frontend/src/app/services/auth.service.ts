1887a0b40be53a50117ebed4a94343b1e755bfda
Tomasz Jąder <tomasz.jader@gmail.com>
2025-01-28 23:18:39 +0100

    reactoring note-form section

--- notatnik-frontend/src/app/services/auth.service.ts
+++ notatnik-frontend/src/app/services/auth.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Auth, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithEmailLink, updateProfile} from '@angular/fire/auth';
+import {
+  Auth,
+  authState,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithEmailLink,
+  updateProfile,
+} from '@angular/fire/auth';
 import { from, switchMap } from 'rxjs';
 import { NoteService } from './note.service';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-    
-curentUser$ = authState(this.auth);
-  constructor(private auth: Auth,private  noteService: NoteService) {}
-
+  curentUser$ = authState(this.auth);
+  constructor(private auth: Auth, private noteService: NoteService) {}
 
   login(username: string, password: string) {
     return from(signInWithEmailAndPassword(this.auth, username, password));
@@ -22,10 +26,8 @@ curentUser$ = authState(this.auth);
   }
 
   signUp(email: string, password: string, id: any) {
-    
-        return from(createUserWithEmailAndPassword(this.auth, email, password)).pipe(
-            switchMap(({ user }) => updateProfile(user, { displayName: id}))
-          );
-      
+    return from(
+      createUserWithEmailAndPassword(this.auth, email, password)
+    ).pipe(switchMap(({ user }) => updateProfile(user, { displayName: id })));
   }
 }
